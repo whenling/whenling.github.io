15bfe802d440a363738aaaa107c7e567fba1bb40
ken <ken@NOTE-KXX>
2015-12-20 19:41:00 +0800

    js

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -24,3 +24,14 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$("#aFloatTools_Show").click(function(){
+	$('#divFloatToolsView').animate({width:'show',opacity:'show'},100,function(){$('#divFloatToolsView').show();});
+	$('#aFloatTools_Show').hide();
+	$('#aFloatTools_Hide').show();				
+});
+$("#aFloatTools_Hide").click(function(){
+	$('#divFloatToolsView').animate({width:'hide', opacity:'hide'},100,function(){$('#divFloatToolsView').hide();});
+	$('#aFloatTools_Show').show();
+	$('#aFloatTools_Hide').hide();	
+}); 
